ab07e94e7fbc4600b7ea4c9618fdef336ec9ad09
Max Fedorchuk <98663185+MaxF1996@users.noreply.github.com>
2023-01-01 16:25:13 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 const page = document.querySelector('body');
+let startRainbow = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
